922b17d6e39167cd013c478847f01f53331d2c9b
ZeHuei <zlim11@qub.ac.uk>
2025-04-03 21:14:01 +0100

    add Game BK sound add a speed boost which implemented in game panel add new class sound to store all sound and song Swift.java is the same as other objects

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -103,7 +103,7 @@ public class GamePanel extends JPanel implements Runnable {
         }
 
         player.draw(g2);
-
+        ui.draw(g2);
         g2.dispose();
     }
     public void playMusic(int i){
